3f78dcce25b98bcc3c9825dfe7a9f751a5a9bcac
maniel <manuelpepr99@icloud.com>
2025-11-09 01:35:11 +0000

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number_1 = int(input("Whats the value for first number? "))
+number_2 = int(input("What's the value for second number? "))
+
+add = number_1 + number_2
+sub = number_1 - number_2
+mul = number_1 * number_2
+
+print("Addition of", number_1, "and", number_2, "is", add)
+print("Subtraction of", number_1, "and", number_2, "is", sub)
+print("Multiplication of", number_1, "and", number_2, "is", mul)
