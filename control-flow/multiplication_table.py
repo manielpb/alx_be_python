faf4d6a45a19ba0861458794e1c84a4dca03c577
maniel <manuelpepr99@icloud.com>
2025-11-16 23:27:43 +0000

    multiplication table generator-update

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,8 @@
 number = int(input("Enter a number to see its multiplication table:"))
 arr = [1,2,3,4,5,6,7,8,9,10]
 
-for value in arr:
-    print(f'{number} * {value} = {number * value}')
+for x in range(1,11):
+    print(f'{number} * {x} = {number * x}')
+
+
+
