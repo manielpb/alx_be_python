621d2309a0255f4a8eb778d470123f8240363058
maniel <manuelpepr99@icloud.com>
2025-11-23 21:44:20 +0000

    Shopping List Manager mandatory

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,8 @@ def  perform_operation(num1, num2, operation):
         case "multiply":
             return num1 * num2
         case "divide":
-            if(num2 == 0):
-                return "Cannot divide by zero"
-            elif:
-                return num1/num2
+            if num2 == 0:
+                return "Error: Division by zero"
+            else:
+                return num1 / num2
+            
